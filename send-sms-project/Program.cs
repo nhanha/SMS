0ba005b9e66c834306df367dac7bd991654c29b2
nhanha <havietnhan@gmail.com>
2017-03-25 01:03:56 +0700

    Add files via upload

---
+++ send-sms-project/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sinch.ServerSdk;
+
+namespace Sinch.SendSms.Example
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            var smsApi = SinchFactory.CreateApiFactory("b302dbec-628e-4579-95a0-8e30f9d471b6", "/wViXDfWlUm+Z1jjcLp8DQ==").CreateSmsApi();
+            var sendSmsResponse = await smsApi.Sms("+84918492075", "Hello world.  Sinch SMS here.").Send();
+
+            await Task.Delay(TimeSpan.FromSeconds(10)); // May take a second or two to be delivered.
+
+            var smsMessageStatusResponse = await smsApi.GetSmsStatus(sendSmsResponse.MessageId);
+            Console.WriteLine(smsMessageStatusResponse.Status);
+            Console.ReadLine();
+        }
+    }
+}
